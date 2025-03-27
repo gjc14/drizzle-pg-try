99d1c63068a80cf2cf32bbc0ec5fc68686033d89
gjc14 <yin@yinc.me>
2025-03-28 04:22:33 +0800

    feat: install drizzle

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { db } from "~/db/db.server";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +9,13 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
+export const loader = async ({ request }: Route.LoaderArgs) => {
+  const result = await db.execute("select 1");
+  console.log(result);
+
+  return {};
+};
+
+export default function Home({ loaderData }: Route.ComponentProps) {
   return <Welcome />;
 }
